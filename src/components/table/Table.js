3bd666a04103cc92b4580888b33dd3db1371e773
yeugen1 <yeugen@gmail.com>
2020-09-12 14:23:50 +0200

    different cell can be selected

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,24 +1,43 @@
 import {ExcelComponent} from '@core/ExcelComponent'
+import {$} from "@core/dom";
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from '@/components/table/table.resize'
-import {shouldResize} from '@/components/table/table.functions'
+import {isCell, shouldResize} from '@/components/table/table.functions'
+import {TableSelection} from "@/components/table/TableSelection";
 
 export class Table extends ExcelComponent {
-  static className = 'excel__table'
+	static className = 'excel__table'
 
-  constructor($root) {
-    super($root, {
-      listeners: ['mousedown']
-    })
-  }
+	constructor($root) {
+		super($root, {
+			listeners: ['mousedown']
+		})
+	}
 
-  toHTML() {
-    return createTable(20)
-  }
+	toHTML() {
+		return createTable(20)
+	}
 
-  onMousedown(event) {
-    if (shouldResize(event)) {
-      resizeHandler(this.$root, event)
-    }
-  }
+	prepare() {
+		this.selection = new TableSelection()
+	}
+
+	init() {
+		super.init()
+
+		this.selection = new TableSelection()
+		const $cell = this.$root.find('[data-id="0:0"]')
+		this.selection.select($cell)
+	}
+
+	onMousedown(event) {
+		if (shouldResize(event)) {
+			resizeHandler(this.$root, event)
+		} else if (isCell(event)) {
+			const $target = $(event.target)
+			this.selection.select($target )
+		}
+
+
+	}
 }

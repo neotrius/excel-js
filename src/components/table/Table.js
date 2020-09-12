fe2b70913c1bece4a42a5d2a08e44b1f8339c787
yeugen1 <yeugen@gmail.com>
2020-09-12 17:26:28 +0200

    added cell multiselection

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,7 +2,7 @@ import {ExcelComponent} from '@core/ExcelComponent'
 import {$} from "@core/dom";
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from '@/components/table/table.resize'
-import {isCell, shouldResize} from '@/components/table/table.functions'
+import {matrix, isCell, shouldResize} from '@/components/table/table.functions'
 import {TableSelection} from "@/components/table/TableSelection";
 
 export class Table extends ExcelComponent {
@@ -35,9 +35,17 @@ export class Table extends ExcelComponent {
 			resizeHandler(this.$root, event)
 		} else if (isCell(event)) {
 			const $target = $(event.target)
-			this.selection.select($target )
-		}
+			if (event.shiftKey) {
 
+				const $cells = matrix($target, this.selection.current)
+						.map(id => this.$root.find(`[data-id="${id}"]`))
+				this.selection.selectGroup($cells)
+
+
+			} else {
+				this.selection.select($target)
+			}
+		}
 
 	}
 }
